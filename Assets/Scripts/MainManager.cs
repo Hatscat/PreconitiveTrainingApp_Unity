e1e1c86b92337b231661bcd468ff317c2311e4d2
Lucien Boudy <luboudy@gmail.com>
2016-03-20 17:17:59 +0100

    first commit

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour
+{
+	public const string FIREBASE_URL = "https://crackling-torch-3895.firebaseio.com"; // "https://crackling-torch-3895.firebaseio.com";
+
+	public static MainManager singleton;
+	public static IFirebase firebase_root;
+	public static IFirebase firebase_users;
+	public static string user_id;
+	public static JSONObject user_data;
+
+	public Transform loginCanvas, clientsCanvas;
+
+	void Awake ()
+	{
+		if (singleton != null)
+		{
+			Destroy(singleton.gameObject);
+		}
+		singleton = this as MainManager;
+
+		firebase_root = Firebase.CreateNew(FIREBASE_URL);
+		firebase_users = firebase_root.Child("users");
+
+		user_id = "";
+
+		//print("firebase_users: " + firebase_users);
+		//firebase_users.Child("bob123").SetValue("test!");
+		//print("key: " + firebase_users.Key);
+	}
+
+	public void SetUserId (string _user_id)
+	{
+		user_id = _user_id;
+		StartCoroutine(EnterTheApp());
+	}
+
+	IEnumerator EnterTheApp ()
+	{
+		yield return StartCoroutine(GetUserData());
+		yield return StartCoroutine(ChangeCameraTarget(clientsCanvas, 1f));
+
+	}
+
+	IEnumerator GetUserData ()
+	{
+		WWW www = new WWW(FIREBASE_URL + "/users/" + user_id + ".json?auth=" + user_id);
+		yield return www;
+
+		Debug.Log(www.text);
+		user_data = new JSONObject(www.text);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			StartCoroutine(ChangeCameraTarget(clientsCanvas, 1f));
+		}
+
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			StartCoroutine(ChangeCameraTarget(loginCanvas, 1f));
+		}
+	}
+
+	IEnumerator ChangeCameraTarget (Transform _target, float transition_duration)
+	{
+		Vector3 initial_position = Camera.main.transform.position;
+		Vector3 final_position = _target.position - _target.forward * 350f;
+		Quaternion initial_rotation = Camera.main.transform.rotation;
+		Quaternion final_rotation = Quaternion.LookRotation(_target.position - final_position, _target.up);
+		float timer = 0f;
+
+		while (timer < transition_duration)
+		{
+			float k = timer / transition_duration;
+
+			Camera.main.transform.position = Vector3.Lerp(initial_position, final_position, k);
+			Camera.main.transform.rotation = Quaternion.Lerp(initial_rotation, final_rotation, k);
+
+			timer += Time.deltaTime;
+			yield return null;
+		}
+		Camera.main.transform.position = final_position;
+		Camera.main.transform.rotation = final_rotation;
+	}
+}
---
+++ Assets/Scripts/UsersManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class UsersManager : MonoBehaviour
+{
+	public static UsersManager singleton;
+	//public static string user_id;
+	public InputField emailInputField, pwdInputField;
+
+	string tmp_debug;
+
+	void Awake ()
+	{
+		if (singleton != null)
+		{
+			Destroy(singleton.gameObject);
+		}
+		singleton = this as UsersManager;
+
+		tmp_debug = "debug...";
+
+		StartCoroutine("getUsers");
+
+		//emailInputField.onValueChange.AddListener(setEmail);
+		//pwdInputField.onValueChange.AddListener(setPwd);
+	}
+/*
+	public void setEmail (string _email)
+	{
+		tmp_debug = "email: " + _email;
+	}
+
+	public void setPwd (string _pwd)
+	{
+	}
+*/
+	public void Login ()
+	{
+		Debug.Log("Login req");
+		MainManager.firebase_root.AuthWithPassword(emailInputField.text, pwdInputField.text, (AuthData auth) =>
+		{
+			Debug.Log("auth success!! " + auth.Uid);
+			tmp_debug = "auth success!! " + auth.Uid;
+			//MainManager.user_id = auth.Uid;
+			MainManager.singleton.SetUserId(auth.Uid);
+
+		}, (FirebaseError e) =>
+		{
+			Debug.Log("auth failure!!");
+			tmp_debug = "auth failure: " + e.Message; 
+		});
+
+	}
+
+	public void Signup ()
+	{
+		Debug.Log("Signup req");
+		//MainManager.firebase_root.Child("users").SetValue(new Dictionary("benwu@google.com", "password"));
+		//signup_email_text.GetComponent<Text>
+
+#if UNITY_ANDROID 
+		MainManager.firebase_root.CreateUser(emailInputField.text, pwdInputField.text, () =>
+		{
+			Debug.Log("User created");
+			tmp_debug = "User created";
+			Login();
+		}, (FirebaseError e) =>
+		{
+			Debug.Log("user creation fail!!");
+			tmp_debug = "user creation fail: " + e.Message; 
+		});
+#endif
+
+
+	}
+
+
+	public void Logout ()
+	{
+
+	}
+
+	void OnGUI ()
+	{
+		GUI.Box(new Rect(Screen.width * .05f, Screen.height * .05f, Screen.width * .9f, Screen.height * .2f), tmp_debug);
+	}
+
+	IEnumerator getUsers ()
+	{
+		WWW www = new WWW("https://crackling-torch-3895.firebaseio.com/users.json");
+		yield return www;
+
+		Debug.Log(www.text);
+		tmp_debug = "users: " + www.text;
+	}
+}
